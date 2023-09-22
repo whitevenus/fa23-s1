88ed5707a17c5cfb0c5ef1d33849753b5e20e5e3
whitevenus <2153710263@qq.com>
2023-09-22 08:40:11 +0800

    finished lab02

--- lab02/src/bomb/BombMain.java
+++ lab02/src/bomb/BombMain.java
@@ -1,6 +1,11 @@
 package bomb;
 
 import common.IntList;
+import edu.princeton.cs.algs4.In;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 public class BombMain {
     public static void main(String[] args) {
@@ -11,13 +16,27 @@ public class BombMain {
         // TODO: Find the correct inputs (passwords) to each phase using debugging techniques
         Bomb b = new Bomb();
         if (phase >= 0) {
-            b.phase0("Figure this out. I wonder where the phases are defined...");
+            //b.phase0("Figure this out. I wonder where the phases are defined...");
+            b.phase0("39291226");
         }
         if (phase >= 1) {
-            b.phase1(null); // Figure this out too
+            //b.phase1(null); // Figure this out too
+            IntList pwd = IntList.of(0, 9, 3, 0, 8);
+            b.phase1(pwd);
         }
+
         if (phase >= 2) {
-            b.phase2("Figure this out. I wonder where the phases are defined...");
+            Random r = new Random(1337);
+            Set<Integer> numbers = new HashSet<>();
+            while (numbers.size() < 100000) {
+                numbers.add(r.nextInt());
+            }
+
+            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < 1338; i++) {
+                str.append(numbers.toArray()[i]).append(" ");
+            }
+            b.phase2(str.toString());
         }
     }
 }
